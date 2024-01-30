2b4164c0d985152430fa86d7594d45a455d30bcc
shantanu <jauntyman1@gmail.com>
2024-01-30 17:42:20 +0530

    adding features

--- starter.py
+++ starter.py
@@ -1,3 +1,20 @@
+import os
+import yaml
+from configfile import config
 from userauth import generatekey
 call = generatekey.run()
 call.generate_uid()
+mongo_path = os.path.join(os.path.dirname(__file__),"userauth","mn.env")
+jwy_path = os.path.join(os.path.dirname(__file__),"userauth",".env")
+yamlformat = {
+    "ENV":{
+        "MONGOENV":mongo_path,
+        "JWTENV":jwy_path
+    },
+    "ALGO":'HS256',
+    "COLLECTIONS":{
+        "DATACOLLECTION":"userdata",
+        "USERCREDENTIALSCOLLECTION":"usercreds"
+
+    }
+}
