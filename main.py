1f94ba537fef86c188833bb5bc358d78eac15c20
shantanu <jauntyman1@gmail.com>
2024-01-30 16:16:32 +0530

    commit-2

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from routes.route import router
 
 app = FastAPI()
 app.include_router(router)
+#this is our homepage
 @app.get('/')
 async def homePage():
     return {"homepage"}
+@app.get('/home')
+async def home():
+    return {"----homeURL----"}
 if __name__ == "__main__":
     uvicorn.run(app=app,host='127.0.0.1',port=8000)
