f397b26b789599e960474c3430b7d262a7e2e830
jaunty-jaswal <98932147+jaunty-jaswal@users.noreply.github.com>
2024-01-30 19:32:38 +0530

    creating-folder-file

--- starter.py
+++ starter.py
@@ -4,25 +4,32 @@ import yaml
 from userauth import generatekey
 call = generatekey.run()
 call.generate_uid()
-mongo_path = os.path.join(os.path.dirname(__file__),"userauth","mn.env")
+mongo_path = os.path.abspath(os.path.join(os.path.dirname(__file__),"userauth","mn.env"))
 jwy_path = os.path.join(os.path.dirname(__file__),"userauth",".env")
+collectiondata= ["userdata","usercreds"]
 yamlformat = {
     "ENV":{
-        "MONGOENV":mongo_path,
-        "JWTENV":jwy_path
+        'MONGOENV':mongo_path,
+        'JWTENV':jwy_path
     },
-    "ALGO":'HS256',
-    "COLLECTIONS":{
-        "DATACOLLECTION":"userdata",
-        "USERCREDENTIALSCOLLECTION":"usercreds"
-
+    'ALGO': "HS256",
+    'COLLECTIONS':{
+        "DATACOLLECTION": str(collectiondata[0]),
+        "USERCREDENTIALSCOLLECTION": str(collectiondata[1]),
+        
     }
 }
 filepath = os.path.abspath(os.path.dirname(__file__))
-folder = os.mkdir(os.path.join(filepath,"config"))
-if folder:
-    shutil.rmtree(folder)
-    os.mkdir(os.path.join(filepath,"config"))
-else :
-    os.mkdir(os.path.join(filepath,"config"))
+if os.path.exists(os.path.abspath(os.path.join(os.path.dirname(__file__),"config"))):
+    shutil.rmtree(os.path.abspath(os.path.join(os.path.dirname(__file__),"config")))
+
+os.mkdir(os.path.join(filepath,"config"))
+folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__),"config"))
+file = os.path.join(os.path.dirname(__file__),"config","config.yaml")
+
+with open(file,"w+") as f:
+    yaml.dump(yamlformat,f,allow_unicode=False,default_flow_style=False)
+
+ 
+
 
