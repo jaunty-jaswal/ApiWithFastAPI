f388554bb9bd2832a035a9565e238f5359c1330d
jaunty <jauntyman1@gmail.com>
2024-01-30 09:33:54 -0500

    automatedfoldercreation

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 <<comment
 bash Entrypoint.sh
+
 comment
+#if u get error, do this
+#sudo apt install python3.10-venv
 
 python3 -m venv .venv
-source .venv/bin/activate
+source .venv/bin/activate.csh
 pip install -r requirements.txt
 python3 starter.py
 python3 main.py
--- database/pathsetter/setpath.py
+++ database/pathsetter/setpath.py
@@ -3,7 +3,7 @@ import yaml
 
 class pathset:
     def __init__(self):
-        self.path = os.path.abspath(os.path.join(os.path.dirname(__file__),"..","..","configfile","config.yaml"))
+        self.path = os.path.abspath(os.path.join(os.path.dirname(__file__),"..","..","config","config.yaml"))
     def openyaml(self):
         with open(self.path,"r") as f:
             config = yaml.full_load(f)
