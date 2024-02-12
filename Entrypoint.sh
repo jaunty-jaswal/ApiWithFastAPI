445a82b36f65f05b781d4fb4e747fc0962a2b00f
shantanu <jauntyman1@gmail.com>
2024-02-12 16:24:17 +0530

    minor changes

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -23,14 +23,15 @@ sudo apt install libespeak1
 python3 -m venv .venv
 source .venv/bin/activate.csh
 pip install -r requirements.txt
-# cd ..
-# cd kafka-server
-# gnome-terminal --tab --title="Zookeeper-server" --execute ./bin/zookeeper-server-start.sh ./config/zookeeper.properties
-# pid1=$$
-# gnome-terminal --tab --title="kafka-server" --execute ./bin/kafka-server-start.sh ./config/server.properties
-# pid2=$$
-# cd ..
-# cd ApiWithFastAPI
+cd ..
+cd kafka-server
+gnome-terminal --tab --title="Zookeeper-server" --execute ./bin/zookeeper-server-start.sh ./config/zookeeper.properties
+pid1=$$
+sleep 0.5
+gnome-terminal --tab --title="kafka-server" --execute ./bin/kafka-server-start.sh ./config/server.properties
+pid2=$$
+cd ..
+cd ApiWithFastAPI
 python3 starter.py
 #./kafka-server/bin/zookeeper-server-start.sh ./config/zookeeper.properties
 #./kafka-server/bin/kafka-server-start.sh ./config/server.properties
